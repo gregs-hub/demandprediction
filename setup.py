4b359c715ba477ed695c3daa2f110371feac7b53
DHIgrse <grse@dhigroup.com>
2021-10-25 14:34:39 +0200

    V0.36 add multiple members

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="demandprediction",
-    version='0.3.0',
+    version='0.3.6',
     install_requires=[],
     extras_require={
         "dev": [],
