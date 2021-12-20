cc21f418950c993a9d11e5c9b47f228ccb224b24
DHIgrse <grse@dhigroup.com>
2021-12-20 18:50:43 +0100

    V0.3.19 fix  SELECT * in database

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="demandprediction",
-    version='0.3.18',
+    version='0.3.19',
     install_requires=[],
     extras_require={
         "dev": [],
